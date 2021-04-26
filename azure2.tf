79eee507651d572a7745d60432612c9d65207661
tdsan <ctsanders@gmail.com>
2021-04-26 16:07:55 -0400

    Update azure2.tf

--- azure2.tf
+++ azure2.tf
@@ -92,9 +92,9 @@ locals {
 
 # Create Network Security Group
 resource "azurerm_network_security_group" "nsg" {
-  name					= "${azurerm_resource_group.rg.name}-nsg"
-  location				= azurerm_resource_group.rg.location
-  resource_group_name			= azurerm_resource_group.rg.name
+  	name					= "${azurerm_resource_group.rg.name}-nsg"
+  	location				= azurerm_resource_group.rg.location
+  	resource_group_name			= azurerm_resource_group.rg.name
 }
 
 resource "azurerm_network_security_rule" "ports" {
