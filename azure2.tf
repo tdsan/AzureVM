b457f69735b4065e2339c296305937b1a6f765aa
tdsan <ctsanders@gmail.com>
2021-04-26 16:06:51 -0400

    Updated format of the azure2.tf file

--- azure2.tf
+++ azure2.tf
@@ -98,19 +98,19 @@ resource "azurerm_network_security_group" "nsg" {
 }
 
 resource "azurerm_network_security_rule" "ports" {
-	for_each			= local.ports
-      name     				= each.key
-      priority 				= each.value.pri
-	  direction			= "Inbound"
-	  access			= "Allow"
-	  protocol			= "Tcp"
-	  source_port_range		= "*"
-	  destination_port_range	= each.value.dst
-	  source_address_prefix		= each.value.sap
-	  destination_address_prefix	= "*"
-	  resource_group_name		= azurerm_resource_group.rg.name
-	  network_security_group_name	= azurerm_network_security_group.nsg.name
-	}
+    	for_each			= local.ports
+    	name     			= each.key
+    	priority 			= each.value.pri
+    	direction			= "Inbound"
+    	access				= "Allow"
+    	protocol			= "Tcp"
+    	source_port_range		= "*"
+    	destination_port_range		= each.value.dst
+    	source_address_prefix		= each.value.sap
+    	destination_address_prefix	= "*"
+    	resource_group_name		= azurerm_resource_group.rg.name
+    	network_security_group_name	= azurerm_network_security_group.nsg.name
+}
 
 resource "azurerm_network_interface" "nic" {
 	name 				= "${var.vm_name}-nic"
